ab845f4eeba8f98c04e5cef9dc321d9c505ccf49
Taha Salman <taha.s147@gmail.com>
2019-10-06 22:28:06 -0400

    just checking for TCP now

--- sniffer.py
+++ sniffer.py
@@ -38,16 +38,7 @@ def main():
             print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
             print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
 
-            # ICMP
-            if ipv4.proto == 1:
-                icmp = ICMP(ipv4.data)
-                print(TAB_1 + 'ICMP Packet:')
-                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
-                print(TAB_2 + 'ICMP Data:')
-                print(format_multi_line(DATA_TAB_3, icmp.data))
-
-            # TCP
-            elif ipv4.proto == 6:
+            if ipv4.proto == 6:
                 tcp = TCP(ipv4.data)
                 print(TAB_1 + 'TCP Segment:')
                 print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
@@ -70,13 +61,10 @@ def main():
                             print(format_multi_line(DATA_TAB_3, tcp.data))
                     else:
                         print(TAB_2 + 'TCP Data:')
+                        if tcp.src_port == 443:
+                            print(f'{TAB_2} HTTPS Port: ')
                         print(format_multi_line(DATA_TAB_3, tcp.data))
 
-            # UDP
-            elif ipv4.proto == 17:
-                udp = UDP(ipv4.data)
-                print(TAB_1 + 'UDP Segment:')
-                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port, udp.dest_port, udp.size))
 
             # Other IPv4
             else:
